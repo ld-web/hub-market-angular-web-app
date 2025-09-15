b132a0d927fd8d0d1bf89095dc6e3cd4954a2b0a
ld-web <lucas@ld-web.net>
2025-09-15 10:06:47 +0200

    :bug: fix build errors & warnings

--- src/app/layouts/shop-layout/components/nav/nav.ts
+++ src/app/layouts/shop-layout/components/nav/nav.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
-import { NavLink } from '../nav-link/nav-link';
 import { CartBadgeComponent } from '../cart-badge/cart-badge';
 
 @Component({
   selector: 'app-nav',
-  imports: [NavLink, CartBadgeComponent],
+  imports: [CartBadgeComponent],
   templateUrl: './nav.html',
-  styleUrl: './nav.css'
+  styleUrl: './nav.css',
 })
-export class Nav {
-
-}
+export class Nav {}
